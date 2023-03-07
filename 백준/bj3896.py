5bab54582e0dbbb76a4f27e1dbe0b0711245d7ae
onyoo <dhsdb02@gmail.com>
2023-03-07 09:17:12 +0900

    백준 3896 소수

---
+++ 백준/bj3896.py
@@ -0,0 +1,17 @@
+from sys import stdin
+import math
+
+t = int(stdin.readline())
+
+def isPrime(num):
+    answer = True
+    for i in range(2, math.sqrt(k)):
+        if k % i == 0:
+            answer = False
+    return answer
+
+
+for _ in range(t):
+    k = int(stdin.readline())
+
+
