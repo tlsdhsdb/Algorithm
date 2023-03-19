78f83a6e3f533e4cc2f7491a2aa589850f909bb0
onyoo <dhsdb02@gmail.com>
2023-03-19 17:38:05 +0900

    백준 1260 bfs&dfs

--- 백준/bj1260.py
+++ 백준/bj1260.py
@@ -1,45 +1,53 @@
+'''
+bfs & dfs
+방문할 수 있는 정점이 여러개인 경우 정점 번호가 작은 것을 먼저 방문
+방문 할 점이 없는 경우 종료
+
+'''
+
 from sys import stdin
 from collections import deque
 
-n, m, v = map(int, stdin.readline().split())
+n,m,v = map(int,stdin.readline().split())
+arr = [[] for i in range(n+1)]
+visited_dfs = [False] * (n+1)
+visited_bfs = [False] * (n+1)
 
-# 노드개수,에지개수,시작점
 
-arr = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
+answer_dfs = []
+answer_bfs = []
 
-for _ in range(1, m + 1):
-    start, end = map(int, stdin.readline().split())
-    arr[start].append(end)
-    arr[end].append(start)
+for _ in range(1,m+1):
+    idx,value = map(int,stdin.readline().split())
+    arr[idx].append(value)
+    arr[value].append(idx)
 
 for i in range(n+1):
-    arr[i].sort() #정렬
+    arr[i].sort()
 
 def dfs(node):
-    global arr
-    print('now',node)
-    visited[node] = True
-    print(node)
-    for n in arr[node]:
-        if not visited[i]:
-            dfs(n)
-
-visited = [False] * (n+1) # visited 배열 초기화
-dfs(v)
+    visited_dfs[node] = True
+    answer_dfs.append(node)
+    for i in arr[node]:
+        if not visited_dfs[i]:
+            dfs(i)
 
 def bfs(node):
-    global arr
     que = deque()
     que.append(node)
-    visited[node] = True
+    visited_bfs[node] = True
     while que:
-        now = que.popleft()
-        print('now:',now)
-        for i in arr[now]:
-            if not visited[i]:
-                visited[i] = True
+        now_node = que.popleft()
+        answer_bfs.append(now_node)
+        for i in arr[now_node]:
+            if not visited_bfs[i]:
+                visited_bfs[i] = True
                 que.append(i)
 
+
+
+dfs(v)
 bfs(v)
 
+print(*answer_dfs)
+print(*answer_bfs)
