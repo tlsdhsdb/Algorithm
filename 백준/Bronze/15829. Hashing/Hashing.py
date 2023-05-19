0cbe2292773e7f1149a2b3c3a636b049ee4450cd
On Yoo <dhsdb02@gmail.com>
2023-05-20 01:32:20 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,17 @@
+'''
+항의 번호에 해당하는 만큼 특정한 숫자를 거듭제곱해서 곱한다음 더함
+
+'''
+import math
+
+l = int(input())
+st = list(input())
+
+answer = 0
+
+for i in range(l):
+    num = ord(st[i]) - 96
+    answer += num * int(math.pow(31,i))
+
+print(answer%1234567891)
+
