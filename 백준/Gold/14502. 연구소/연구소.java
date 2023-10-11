58c6088d54b90e11f3eebee0b523a713193a7d2b
On Yoo <dhsdb02@gmail.com>
2023-10-12 08:59:25 +0900

    [Gold IV] Title: 연구소, Time: 788 ms, Memory: 296012 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -11,31 +10,24 @@ import java.util.StringTokenizer;
  * @performance
  * @category
  * @note
- * 연구소의 최대 크기는 8 * 8 = 64
- * 완전 탐색을 할 경우 구해야 할 벽의 위치 경우의 수는  64C3 = 41664
- * 시간제한은 2초로 초당 1억번 * 2 = 총 2억번 가능하고
- * 시간 제한을 넘지 않기 때문에 완전탐색을 사용하여 문제를 푸는 것이 적절하다고 판단함
- * 1. 벽의 위치를 고정합니다 이걸 완전탐색으로 진행합니다
- * 2. 바이러스를 퍼트리는 과정을 탐색을 통하여 진행합니다
- * @see https://www.acmicpc.net/problem/14502
- * @since 2023-08-10
+ * 안전영역의 최대 크기
+ * @see
+ * https://www.acmicpc.net/problem/14502
+ * @since 2023/10/12
  **/
 public class Main {
     static class Point{
-        int x;
-        int y;
+        int x,y;
 
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    static Point[] arrow = {new Point(0,1),new Point(0,-1),new Point(1,0),new Point(-1,0)};
-    //상하좌우를 저장하는 변수
-    static int N,M; // 연구소의 세로,가로
-    static int[][] map; // 연구소의 지도
-    static int safe = Integer.MIN_VALUE; // 안전구역의 최대 크기를 저장하는 변수
-
+    static int N,M;
+    static int[][] map;
+    static Point[] dir = {new Point(0,1),new Point(0,-1),new Point(1,0),new Point(-1,0)};
+    static int max;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
@@ -44,79 +36,74 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
+        max = Integer.MIN_VALUE;
 
-        for(int n = 0; n < N ; n ++){
+        for(int n=0;n<N;n++){
             st = new StringTokenizer(br.readLine()," ");
-            for(int m = 0 ; m < M ; m++){
+            for(int m=0;m<M;m++){
                 map[n][m] = Integer.parseInt(st.nextToken());
             }
         }
         settingWall(0);
-        System.out.print(safe);
+        System.out.printf("%d\n",max);
     }
-
-    static void settingWall(int wall){
-        //dfs 벽이 3개일때까지 확인을 해야하기 때문에
-        if(wall == 3){
-            // 벽이 세개 세워졌으면
-            // 바이러스를 퍼트린다
+    static void settingWall(int depth){
+        if(depth == 3){
+            // 벽을 다 세웠으면
             spreadVirus();
             return;
         }
-
-        for(int n = 0; n < N ; n ++){
-            for(int m = 0 ; m < M ; m++){
-                if(map[n][m] == 0){
-                    map[n][m] = 1; // 벽을 세운다
-                    settingWall(wall+1);
-                    map[n][m] = 0; // 벽을 원상복구한다
-                }
+        for (int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(map[i][j] != 0) continue;
+                map[i][j] = 1;
+                settingWall(depth+1);
+                map[i][j] = 0;
             }
         }
     }
-
     static void spreadVirus(){
-        // bfs 모든 정점을 탐색하며,빈 구역을 확인해야하기 때문에
+        // 바이러스를 퍼트린다
+        int[][] copy = copyMap();
         Queue<Point> que = new ArrayDeque<>();
-        int safeZone = 0; // 안전구역을 저장하는 변수
-
-        for(int n = 0; n < N ; n ++){
-            for(int m = 0 ; m < M ; m++){
-                if(map[n][m] == 2){
-                    que.add(new Point(n,m));
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(copy[i][j] == 2){
+                    que.add(new Point(i,j)); // 시작지점을 미리 큐에 넣어주면 된다
                 }
             }
         }
-        //시작 노드,바이러스의 시작점을 큐에 넣어주기
 
-        int copyMap[][] = new int[N][M];
-        for(int n = 0; n < N; n++) copyMap[n] = map[n].clone();
-
-        // 원본맵이 아닌 복사본에서 바이러스를 퍼트리기
-        // 원본으로 다시 되돌려 놓기 어렵기 때문이다
 
         while(!que.isEmpty()){
-            Point now = que.poll(); // 현재 지점
+            Point curr = que.poll();
+            copy[curr.x][curr.y] = 2; // 바이러스가 퍼짐
+            for(Point d : dir){
+                int nx = curr.x + d.x;
+                int ny = curr.y + d.y;
 
-            for(Point ar : arrow){
-                int nx = ar.x + now.x;
-                int ny = ar.y + now.y;
-
-                if(nx >= N || nx < 0 || ny >= M || ny < 0 ) continue;
-                //유효하지 않은 좌표일 경우 탐색하지 않는다
-                if(copyMap[nx][ny] == 0 ){
-                    // 빈공간일 경우에만 바이러스를 퍼트린다
-                    que.add(new Point(nx,ny));
-                    copyMap[nx][ny] = 2;
-                }
-            }
-            //현재 지점에서 4방향을 탐색함
-        }
-        for(int n = 0; n < N; n++){
-            for(int m = 0 ; m < M; m++){
-                if(copyMap[n][m] == 0) safeZone++;
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(copy[nx][ny] != 0) continue;
+                que.add(new Point(nx,ny));
+                copy[nx][ny] = 2; // 바이러스를 퍼트린다
             }
         }
-        safe = Math.max(safeZone,safe);
+        int sum = 0;
+        for(int n=0;n<N;n++){
+            for(int m=0;m<M;m++){
+                if(copy[n][m] == 0) sum++;
+            }
+        }
+        max = Math.max(sum,max);
+    }
+
+    static int[][] copyMap(){
+        int[][] copy = new int[N][M];
+        for(int n=0;n<N;n++){
+            for(int m=0;m<M;m++){
+                copy[n][m] = map[n][m];
+            }
+        }
+        return copy;
     }
 }
