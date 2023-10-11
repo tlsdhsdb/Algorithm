7be1793572a9a3df459b7a46d0921960a1d7c536
On Yoo <dhsdb02@gmail.com>
2023-10-11 23:34:34 +0900

    [D4] Title: 수영대회 결승전 ( 완전 탐색 + 구현 ), Time: 109 ms, Memory: 20,276 KB -BaekjoonHub

--- SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
+++ SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
@@ -1,50 +1,53 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
  * @author 신온유
- * @git
- * @youtube
  * @performance
  * @category
  * @note
- * @see https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWngfZVa9XwDFAQU#
- * @since 2023-09-15
+ * 가장 빠른길 -> BFS
+ * 장애물 , 소용돌이
+ * 소용돌이는 2초동안 유지 1초동안 잠잠
+ * 몇초만에 골인 ? -> 탐색횟수
+ * @see
+ * https://swexpertacademy.com/main/code/userProblem/userProblemDetail.do?contestProbId=AWKaG6_6AGQDFARV
+ * @since 2023/10/11
  **/
 public class Solution {
     static class Point{
-        int x,y,dist;
+        int x,y;
+        int time;
 
-        public Point(int x, int y, int dist) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
+        }
+
+        public Point(int x, int y, int time) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
         }
     }
-    static int N;
+    static int T,N;
     static int[][] map;
     static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    static int answer;
-
     static Point start,end;
+    static Point[] dir = {new Point(0,1),new Point(0,-1),new Point(1,0),new Point(-1,0)};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());
-
-        for(int t=1;t<T+1;t++) {
+        T = Integer.parseInt(br.readLine());
+        for(int t=1;t<T+1;t++){
             N = Integer.parseInt(br.readLine());
-
             map = new int[N][N];
-            visited = new boolean[N][N];
-
 
             for(int i=0;i<N;i++){
                 st = new StringTokenizer(br.readLine()," ");
@@ -52,24 +55,18 @@ public class Solution {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+            st = new StringTokenizer(br.readLine()," ");
+            start = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
 
             st = new StringTokenizer(br.readLine()," ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            start = new Point(x,y,0);
-
-            st = new StringTokenizer(br.readLine()," ");
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            end = new Point(x,y,0);
-
-            answer = bfs(start);
-            System.out.printf("#%d %d\n",t,answer);
+            end = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+            int answer = bfs();
+            System.out.printf("#%d %d \n",t,answer);
         }
     }
-
-    static int bfs(Point start){
-        Deque<Point> que = new ArrayDeque();
+    static int bfs(){
+        Queue<Point> que = new ArrayDeque<>();
+        visited = new boolean[N][N]; // 탐색할때마다 초기화
         que.add(start);
         visited[start.x][start.y] = true;
 
@@ -77,36 +74,36 @@ public class Solution {
             int size = que.size();
             for(int i=0;i<size;i++){
                 Point curr = que.poll();
+                if(curr.x == end.x && curr.y == end.y) return curr.time;
 
-                if(curr.x == end.x && curr.y == end.y) {
-                    return curr.dist;
-                }
-                for(int j=0;j<4;j++){
-                    int nx = curr.x + dx[j];
-                    int ny = curr.y + dy[j];
-                    int move = curr.dist + 1;
+                for(Point d : dir){
+                    int nx = curr.x + d.x;
+                    int ny = curr.y + d.y;
+                    int time = curr.time + 1;
 
-                    if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                    if(visited[nx][ny] || map[nx][ny] == 1) continue;
-                    //이미 방문했거나, 장애물이 있을 경우
+                    if(nx<0 || ny<0 || nx >= N || ny >= N) continue; // 유효하지 않은 좌표
+                    if(visited[nx][ny] || map[nx][ny] == 1) continue; // 이미 가본곳 or 장애물
 
-                    if(map[nx][ny] == 2){ //소용돌이가 있는 경우
-                        if(2 == (curr.dist % 3)){
-                            //이 조건을 만족하면,소용돌이가 사라진 것
-                            que.offer(new Point(nx,ny,move));
+                    if(map[nx][ny] == 2){
+                        //소용돌이를 만낫다
+                        if(curr.time % 3 == 2) {
                             visited[nx][ny] = true;
-                        }else{
-                            //소용돌이를 통과하지 못한 경우
-                            //소용돌이가 사라질때까지 기다린다
-                            que.offer(new Point(curr.x, curr.y,curr.dist+1));
+                            que.add(new Point(nx,ny,time)); // 소용돌이를 탈출한다
+                        }
+                        else{
+                            que.add(new Point(curr.x,curr.y,time));
+                            //소용돌이를 탈출하지 못할경우
                         }
                         continue;
                     }
+                    //소용돌이를 만나지 않았을 경우
                     visited[nx][ny] = true;
-                    que.add(new Point(nx,ny,move));
+                    que.add(new Point(nx,ny,time));
                 }
+
             }
+
         }
-        return -1;
+        return -1; // 빠져나가지 못하고 탐색이 끝날 경우
     }
 }
