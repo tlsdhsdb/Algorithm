2b40663a2221e3536821479814d36d72ecf25932
On Yoo <dhsdb02@gmail.com>
2023-10-11 21:23:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 2,357 ms, Memory: 110,832 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -9,95 +9,93 @@ import java.util.StringTokenizer;
  * @performance
  * @category
  * @note
- * 조합 + 완전탐색 문제
- * 1. 약품을 주입 할 위치를 정한다. 약품의 개수만큼 뽑아서 약품을 주입할 위치를 가진 배열을 가져온다
- * 2. 약품을 주입한다
- * 3. 전체 값 - 3번 연속인 횟수
+ * 성능검사 -> 동일한 특성의 셀들이 K(합격기준)개 이상 연속적으로 있는 경우 통과함
+ * 약품투약 -> 하나의 특성으로 한줄이 변함
+ * 최소 투입 횟수를 구하자
+ * 1.약품을 어디에 투입할지 조합을 구한다
+ * 2.성능검사를 약품을 투약할때마다 시행한다
  * @see
- * https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5V1SYKAaUDFAWu
- * @since 2023/10/06
+ * https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5V1SYKAaUDFAWu&categoryId=AV5V1SYKAaUDFAWu&categoryType=CODE&&&
+ * @since 2023/10/11
  **/
 public class Solution {
     static int T,D,W,K;
-    static int answer;
     static int[][] film;
-    static int[] arr; // 약품 주입
-    static boolean[] visit; // 약품 주입 할 위치를 찾는 방문배열
+    static int[] arr; // 주입할 위치를 정하는 배열
+    static int answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         T = Integer.parseInt(br.readLine());
-
         for(int t=1;t<T+1;t++){
             st = new StringTokenizer(br.readLine()," ");
+
             D = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken()); // 통과기준
 
             film = new int[D][W];
             arr = new int[D];
-            answer = Integer.MAX_VALUE;
+            answer = Integer.MAX_VALUE; // 최소값
 
-            for(int d = 0; d < D; d++){
+            for(int i=0;i<D;i++){
                 st = new StringTokenizer(br.readLine()," ");
-                for(int w = 0 ; w < W; w++){
-                    film[d][w] = Integer.parseInt(st.nextToken());
+                for(int j=0;j<W;j++){
+                     film[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            Arrays.fill(arr,-1); // 기본은 아무것도 안채움 상태
+            Arrays.fill(arr,-1);
 
             if(!isPass()) dfs(0,0);
             else answer = 0;
-
-            System.out.printf("#%d %d \n",t,answer);
+            System.out.printf("#%d %d\n",t,answer);
         }
 
     }
     static void dfs(int depth,int idx){
+        //idx 약품 주입횟수
+        //depth 약품을 넣는 위치
         if(idx >= answer) return;
-        //약품을 최소갯수보다 많이 사용하면 그냥 리턴
-        //depth 는 깊이
-        //idx 약품 주입 횟수
+        // 약품을 최솟값보다 많이 사용했다면 볼것도 없이 넘긴다.
         if(depth == D){
-            // 모두 주입했다 !
             if(isPass()) answer = Math.min(answer,idx);
             return;
         }
-        //주입하지 않음
         arr[depth] = -1;
         dfs(depth+1,idx);
-        
+        //아무것도 주입하지 않음
 
-        //A를 주입함
         arr[depth] = 0;
         dfs(depth+1,idx+1);
+        //0번 약물 주입
 
-        //B를 주입함
         arr[depth] = 1;
         dfs(depth+1,idx+1);
+        //1번 약물 주입
 
     }
-
-    private static boolean isPass(){
+    static boolean isPass(){
         int[][] tmp = new int[D][W];
-        for(int d = 0; d < D; d++){
-            int pill = arr[d];
-            for(int w = 0 ; w < W; w++){
-                if(pill != -1) tmp[d][w] = pill;
-                else{
-                    tmp[d][w] = film[d][w];
-                }
+        // 원본값을 손대면 안된다
+        // 검사만 하고 되돌려놓아야 하기때문에
+        for(int i=0;i<D;i++){
+            int pill = arr[i];
+            //한줄씩 채워진다
+            for(int j=0;j<W;j++){
+                if(pill != -1) tmp[i][j] = pill; // 주입하는 경우
+                else tmp[i][j] = film[i][j]; // 주입안하는 경우
             }
         }
-        int width  = W;
-        for(int w = 0; w < W; w++){
+        // 약물 주입
+        int width = W;
+        for(int w=0;w<W;w++){
+            int ex = tmp[0][w]; // 첫 필름 저장
             int cnt = 1;
-            int ex = tmp[0][w]; // 비교할 이전 값
-            for(int d = 1; d < D ; d++){
+            for(int d=1;d<D;d++){
                 if(ex == tmp[d][w]) cnt++;
                 else {
-                    cnt = 1;
+                    cnt = 1; //ex 값이 하나 있기 때문에 1로 초기화
                     ex = tmp[d][w];
                 }
                 if(cnt == K) {
@@ -105,7 +103,7 @@ public class Solution {
                     break;
                 }
             }
-
+            if(width != W - (w + 1)) return false;
         }
         return width == 0;
     }
