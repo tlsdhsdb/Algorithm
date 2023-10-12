928e702f1375ec241d5dfeec3853e24ccaf1f23e
On Yoo <dhsdb02@gmail.com>
2023-10-12 15:16:10 +0900

    [D4] Title: 수영대회 결승전 ( 완전 탐색 + 구현 ), Time: 116 ms, Memory: 19,716 KB -BaekjoonHub

--- SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
+++ SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,23 +6,17 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * @author 신온유
+ * @author onyoo
  * @performance
  * @category
  * @note
- * 가장 빠른길 -> BFS
- * 장애물 , 소용돌이
- * 소용돌이는 2초동안 유지 1초동안 잠잠
- * 몇초만에 골인 ? -> 탐색횟수
  * @see
- * https://swexpertacademy.com/main/code/userProblem/userProblemDetail.do?contestProbId=AWKaG6_6AGQDFARV
- * @since 2023/10/11
+ * @since 2023-10-12
  **/
 public class Solution {
     static class Point{
         int x,y;
         int time;
-
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
@@ -37,18 +30,17 @@ public class Solution {
     }
     static int T,N;
     static int[][] map;
-    static boolean[][] visited;
     static Point start,end;
-    static Point[] dir = {new Point(0,1),new Point(0,-1),new Point(1,0),new Point(-1,0)};
+    static Point[] dir = {new Point(1,0),new Point(-1,0),new Point(0,1),new Point(0,-1)};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         T = Integer.parseInt(br.readLine());
+
         for(int t=1;t<T+1;t++){
             N = Integer.parseInt(br.readLine());
             map = new int[N][N];
-
             for(int i=0;i<N;i++){
                 st = new StringTokenizer(br.readLine()," ");
                 for(int j=0;j<N;j++){
@@ -60,50 +52,47 @@ public class Solution {
 
             st = new StringTokenizer(br.readLine()," ");
             end = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+
             int answer = bfs();
             System.out.printf("#%d %d \n",t,answer);
         }
     }
     static int bfs(){
         Queue<Point> que = new ArrayDeque<>();
-        visited = new boolean[N][N]; // 탐색할때마다 초기화
+        boolean[][] visited = new boolean[N][N];
         que.add(start);
-        visited[start.x][start.y] = true;
+        visited[start.x][start.y] = true; //첫번째 노드 방문
 
         while(!que.isEmpty()){
             int size = que.size();
             for(int i=0;i<size;i++){
                 Point curr = que.poll();
                 if(curr.x == end.x && curr.y == end.y) return curr.time;
+                for(Point p : dir){
+                    int nx = curr.x + p.x;
+                    int ny = curr.y + p.y;
+                    int time = curr.time;
 
-                for(Point d : dir){
-                    int nx = curr.x + d.x;
-                    int ny = curr.y + d.y;
-                    int time = curr.time + 1;
-
-                    if(nx<0 || ny<0 || nx >= N || ny >= N) continue; // 유효하지 않은 좌표
-                    if(visited[nx][ny] || map[nx][ny] == 1) continue; // 이미 가본곳 or 장애물
-
+                    if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue; // 유효하지 않은 좌표일 경우 못간다
+                    if(map[nx][ny] == 1 || visited[nx][ny]) continue; // 장애물일 경우 못간다
                     if(map[nx][ny] == 2){
-                        //소용돌이를 만낫다
-                        if(curr.time % 3 == 2) {
+                        if(time % 3 == 2){
+                            que.add(new Point(nx,ny,time+1));
                             visited[nx][ny] = true;
-                            que.add(new Point(nx,ny,time)); // 소용돌이를 탈출한다
-                        }
-                        else{
-                            que.add(new Point(curr.x,curr.y,time));
-                            //소용돌이를 탈출하지 못할경우
+                            // 소용돌이가 없어지는 시간일 경우
+                        }else{
+                            que.add(new Point(curr.x, curr.y,time+1));
+                            //소용돌이가 계속 있을 경우
+                            //자리에서 시간만 보낸다
                         }
                         continue;
-                    }
-                    //소용돌이를 만나지 않았을 경우
+                    }//소용돌이를 마주한 경우
+                    que.add(new Point(nx,ny,time+1));
                     visited[nx][ny] = true;
-                    que.add(new Point(nx,ny,time));
+                    //장애물 , 소용돌이가 없는 경우 그냥 간다
                 }
-
             }
-
         }
-        return -1; // 빠져나가지 못하고 탐색이 끝날 경우
+        return -1;
     }
 }
