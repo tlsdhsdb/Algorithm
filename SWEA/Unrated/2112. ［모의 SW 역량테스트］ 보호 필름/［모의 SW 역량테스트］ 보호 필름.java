0d21610b9c2a0c1164e3b2161e8167498c39806a
On Yoo <dhsdb02@gmail.com>
2023-10-12 16:02:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 3,892 ms, Memory: 110,584 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -5,106 +5,107 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * @author 신온유
+ * @author onyoo
  * @performance
  * @category
  * @note
- * 성능검사 -> 동일한 특성의 셀들이 K(합격기준)개 이상 연속적으로 있는 경우 통과함
- * 약품투약 -> 하나의 특성으로 한줄이 변함
- * 최소 투입 횟수를 구하자
- * 1.약품을 어디에 투입할지 조합을 구한다
- * 2.성능검사를 약품을 투약할때마다 시행한다
  * @see
- * https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5V1SYKAaUDFAWu&categoryId=AV5V1SYKAaUDFAWu&categoryType=CODE&&&
- * @since 2023/10/11
+ * @since 2023-10-12
  **/
 public class Solution {
     static int T,D,W,K;
     static int[][] film;
-    static int[] arr; // 주입할 위치를 정하는 배열
-    static int answer;
+    static boolean[] visited; //방문배열
+    static int[] selected; // 어떤 약물을 어디에 주입할지 저장하는 배열
+    static int min;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         T = Integer.parseInt(br.readLine());
         for(int t=1;t<T+1;t++){
             st = new StringTokenizer(br.readLine()," ");
 
-            D = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken()); // 통과기준
+            D = Integer.parseInt(st.nextToken()); // 필름의 두께
+            W = Integer.parseInt(st.nextToken()); // 가로크기
+            K = Integer.parseInt(st.nextToken()); // 합격기준
 
             film = new int[D][W];
-            arr = new int[D];
-            answer = Integer.MAX_VALUE; // 최소값
 
-            for(int i=0;i<D;i++){
+            selected = new int[D];
+            visited = new boolean[D];
+            Arrays.fill(selected,-1); // 아무것도 주입하지 않은 상태를 -1로 둠
+            //약물 주입 부분집합을 구하는데 필요함
+
+            for(int d=0;d<D;d++){
                 st = new StringTokenizer(br.readLine()," ");
-                for(int j=0;j<W;j++){
-                     film[i][j] = Integer.parseInt(st.nextToken());
+                for(int w=0;w<W;w++){
+                    film[d][w] =  Integer.parseInt(st.nextToken());
                 }
             }
-            Arrays.fill(arr,-1);
 
-            if(!isPass()) dfs(0,0);
-            else answer = 0;
-            System.out.printf("#%d %d\n",t,answer);
+            min = Integer.MAX_VALUE; // 주입할 약물의 최소개수를 저장할 변수
+
+            if(!check()) dfs(0,0);
+            else min = 0;
+            System.out.printf("#%d %d \n",t,min);
         }
-
     }
-    static void dfs(int depth,int idx){
-        //idx 약품 주입횟수
-        //depth 약품을 넣는 위치
-        if(idx >= answer) return;
-        // 약품을 최솟값보다 많이 사용했다면 볼것도 없이 넘긴다.
+    static void dfs(int idx,int depth){
+        // 주입할 약물의 개수
+        // 약물의 위치
+        if(min < idx) return;
+        // 최소개수보다 많으면 그냥 볼것도 없이 리턴한다
         if(depth == D){
-            if(isPass()) answer = Math.min(answer,idx);
+            // 최대 D까지 탐색
+            if(check()) min = Math.min(min,idx);
             return;
         }
-        arr[depth] = -1;
-        dfs(depth+1,idx);
-        //아무것도 주입하지 않음
+        // 부분집합을 구한다
 
-        arr[depth] = 0;
-        dfs(depth+1,idx+1);
-        //0번 약물 주입
+        selected[depth] = -1;
+        dfs(idx,depth+1);
+        // 아무것도 주입하지 않는다
 
-        arr[depth] = 1;
-        dfs(depth+1,idx+1);
-        //1번 약물 주입
+        selected[depth] = 0;
+        dfs(idx+1,depth+1);
+        // A약물을 주입한다
+
+        selected[depth] = 1;
+        dfs(idx+1,depth+1);
+        // B약물을 주입한다
 
     }
-    static boolean isPass(){
-        int[][] tmp = new int[D][W];
-        // 원본값을 손대면 안된다
-        // 검사만 하고 되돌려놓아야 하기때문에
+    static boolean check(){
+        int[][] copy = new int[D][W];
         for(int i=0;i<D;i++){
-            int pill = arr[i];
-            //한줄씩 채워진다
+            int inject = selected[i];
             for(int j=0;j<W;j++){
-                if(pill != -1) tmp[i][j] = pill; // 주입하는 경우
-                else tmp[i][j] = film[i][j]; // 주입안하는 경우
+                if(inject != -1) copy[i][j] = inject;
+                else copy[i][j] = film[i][j]; // -1은 주입하지 않는 것이기 때문에 원래 필름을 가져온다
             }
         }
-        // 약물 주입
-        int width = W;
+        // 약품을 주입하고
+        int width = W; // 성공할때마다 체크 할 변수
         for(int w=0;w<W;w++){
-            int ex = tmp[0][w]; // 첫 필름 저장
-            int cnt = 1;
+            int ex = copy[0][w];
+            int c = 1;
             for(int d=1;d<D;d++){
-                if(ex == tmp[d][w]) cnt++;
-                else {
-                    cnt = 1; //ex 값이 하나 있기 때문에 1로 초기화
-                    ex = tmp[d][w];
+                int curr = copy[d][w];
+                if(ex == curr) c++;
+                else{
+                    ex = curr; // 현재 값으로 변경해준다
+                    c = 1; // 카운트 변수를 초기화 한다
                 }
-                if(cnt == K) {
+                if(c == K) {
                     width--;
                     break;
                 }
             }
-            if(width != W - (w + 1)) return false;
+            if(width != W - (w+1)) return false;
         }
-        return width == 0;
+        return true;
+        // 체크해준다
     }
+
 }
