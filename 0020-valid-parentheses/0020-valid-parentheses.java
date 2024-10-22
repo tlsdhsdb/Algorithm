d4b91e0682502bd77c1065fa2fffbdff45699ab3
On Yoo <dhsdb02@gmail.com>
2024-10-22 16:15:19 +0900

    Time: 2 ms (60.02%), Space: 42.1 MB (7.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,47 +1,31 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        char[] ch = s.toCharArray();
 
-        if(s.length() % 2 == 1) return false;
+        // 문자열의 각 문자를 순회
+        for (char c : s.toCharArray()) {
+            // 여는 괄호일 경우 스택에 넣기
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                // 닫는 괄호인데 스택이 비어있으면 짝이 맞지 않음
+                if (stack.isEmpty()) {
+                    return false;
+                }
 
-        for(char c : ch){
-            if(c == '(' || c == '[' || c == '{') stack.add(c);
-            //여는 괄호는 스택에 넣기
-            else{
-                if(stack.isEmpty()) return false;
-                //닫는 괄호가 나올 경우 
-                switch (c) {
-                    case ')' : 
-                    if(stack.peek() == '(') {
-                        stack.pop();
-                        break;
-                    }
-                    else return false;
-                    
-                    
-                    case ']' : 
-                    if(stack.peek() == '[') {
-                        stack.pop();
-                        break;
-                    }
-                    else return false;
-                    
-                    
-                    case '}' : 
-                    if(stack.peek() == '{') {
-                        stack.pop();
-                        break;
-                    }
-                    
-                    else return false;
+                // 스택의 마지막 값과 현재 닫힌 괄호가 맞는지 확인
+                char open = stack.pop();
+                if ((c == ')' && open != '(') || 
+                    (c == '}' && open != '{') || 
+                    (c == ']' && open != '[')) {
+                    return false;
                 }
             }
         }
 
-        if(!stack.isEmpty()) return false;
-        
-
-        return true;
+        // 모든 괄호 처리 후 스택이 비어있어야 함
+        return stack.isEmpty();
     }
 }
