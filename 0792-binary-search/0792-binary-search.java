c1c0488dce63cbf7a8a3ec25dc375cffd717e7c9
On Yoo <dhsdb02@gmail.com>
2024-10-23 16:04:45 +0900

    Time: 3 ms (10.83%), Space: 46 MB (8.24%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0;
+        int k = nums.length;
+        int j = 0;
+
+        while(i < k){
+            j =  (i + k)/2;
+
+            System.out.println(nums[j]);
+
+            if(nums[j] < target) i = j + 1;
+            if(nums[j] > target) k = j ;
+            if(nums[j] == target) break;
+        }
+
+        return nums[j] == target ? j : -1;
+    }
+}
