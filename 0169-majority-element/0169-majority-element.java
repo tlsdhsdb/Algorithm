1eda16c52410db325e64982107ad1afc33b56334
On Yoo <dhsdb02@gmail.com>
2024-10-27 15:02:31 +0900

    Time: 3 ms (63.82%), Space: 53.6 MB (9.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,16 @@
-class Solution {
+public class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-
-        int target = 0;
+        
         int count = 0;
-        int n = nums.length;
+        Integer candidate = null;
 
-        for(int num : nums){
-            if(num != target) {
-                target = num;
-                count+=1;
-            }else{
-                count +=1;
-                if(count > n/2) break;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;  // 새로운 후보 선택
             }
+            count += (num == candidate) ? 1 : -1;
         }
-        return target;
+
+        return candidate;
     }
 }
